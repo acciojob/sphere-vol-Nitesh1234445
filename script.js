3991faba60f1d006ead7862f17ef455c1fa14e9a
Nitesh Dhakad <110675128+Nitesh1234445@users.noreply.github.com>
2025-11-04 10:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	    event.preventDefault();
+
+	let rad = parseFloat(document.getElementById('radius').value);
+	let vol = document.getElementById('volume');
+
+	if(isNaN(rad) || rad<0){
+		vol.value = 'NaN';
+		return;
+	}
+	let v = (4/3) * Math.PI * Math.pow(rad , 3);
+	vol.value = v.toFixed(4);
 } 
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
